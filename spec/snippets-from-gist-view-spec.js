0288e5ae9279afdfe2cf3ae395a7083a34770ab3
Steve Mak <steave.mkm@hotmail.com>
2018-07-15 17:39:50 +0800

    Fix path seprator problem on Linux/Mac.

--- spec/snippets-from-gist-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import SnippetsFromGistView from '../lib/snippets-from-gist-view';
-
-describe('SnippetsFromGistView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
